0f34edcb580ef46f8360af1a4157f628e2e1cfb8
Matthias <campe@berlinale.de>
2023-06-26 15:57:53 +0200

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import './App.css';
+import { Flex } from '@chakra-ui/react';
 
 
 function App() {
   
   return (
     <>
+    <Flex h='50px' px='3' alignItems={'center'} borderBottom={'1px'} borderColor={'gray.100'} mb={'50px'}>
+      <nav>
+      <NavLink 
+        className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "active" : ""
+      }
+      to="/video">Video Player</NavLink>
+      <NavLink 
+        className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "active" : ""
+      }
+      to="/meme">Meme Generator</NavLink>
+            </nav>
+    </Flex>
     <main>
     <Outlet />
     </main>
